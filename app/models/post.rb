24eb8755d0957b9f52d0d36db4beb6084cc532e8
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-02 20:00:43 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :content, length: { minimum: 100 }
   validates :category, { in: %w(Fiction Non-Fiction),
-    message: "%{value} is not a valid size" }
+    message: "%{value} is not a valid category" }
 end
