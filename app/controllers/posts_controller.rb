ce6d1970550fe3bfc272e259e2fc2ecc352136e7
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-02 20:11:56 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,12 +8,9 @@ class PostsController < ApplicationController
   end
 
   def update
-    if @post.valid?
-      @post.update
-      redirect_to post_path(@post)
-    else
-      render :edit
-    end
+    @post.update(post_params)
+
+    redirect_to post_path(@post)
   end
 
   private
